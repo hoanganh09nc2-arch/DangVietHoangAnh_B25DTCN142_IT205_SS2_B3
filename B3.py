d6dad147965743b68b7f814e9416907075737ec4
hoanganh09nc2-arch <hoanganh09nc2@gmail.com>
2026-05-19 20:54:05 +0700

    Add files via upload

---
+++ B3.py
@@ -0,0 +1,14 @@
+name_patient = input("Nhập tên bệnh nhân:")
+age_patient = int(input("Nhập tuổi bệnh nhân:"))
+
+
+if age_patient > 120:
+    print("Tuổi không hợp lệ!!")
+elif age_patient >= 80:
+    print("ƯU TIÊN: Người cao tuổi - Hỗ trợ xe lăn, chuyển phòng khám Lão khoa.")
+elif age_patient >= 6:
+    print("KHÁM THƯỜNG: Vui lòng lấy số thứ tự và chờ tới lượt tại sảnh.")
+elif age_patient > 0:
+    print("ƯU TIÊN: Bệnh nhi - Chuyển thẳng phòng khám Nhi.")
+else:
+    print("Tuổi không hợp lệ!!")
